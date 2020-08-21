6363603da6959071f7e7111092b68a27cfb8309c
tx-haixiang <haixiangyan@tencent.com>
2020-08-21 10:55:41 +0800

    feat: 完成拍一拍效果

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,4 +1,4 @@
-import React, {FC, useState} from 'react'
+import React, {FC, useRef, useState} from 'react'
 import styled from "styled-components"
 import './App.css'
 
@@ -14,22 +14,39 @@ const Container = styled.div`
 const Avatar = styled.img`
   width: 120px;
   height: 120px;
-  border-radius: 4px;
-  animation: 1s ease-in-out swing;
+  border-radius: 12px;
+  cursor: pointer;
 `
 
 const PatPat = styled.p`
   color: #5e5e5e;
 `
 
+let clock: number | null = null;
+
 const App: FC = () => {
   const [patter] = useState<string>('海怪')
-  const [pattee] = useState<string>('三个儿子')
+  const [pattee] = useState<string>('懂王')
   const [content] = useState<string>('脸')
+
+  const avatarRef = useRef<HTMLDivElement | null>(null);
+
+  const onPat = () => {
+    if (avatarRef === null || clock !== null) return
+
+    avatarRef.current!.className = 'swing';
+
+    clock = setTimeout(() => {
+      avatarRef.current!.className = '';
+      clock = null;
+    }, 1000)
+  }
 
   return (
     <Container>
-      <Avatar src={require('../assets/avatar.png')} alt="头像"/>
+      <div ref={avatarRef} onDoubleClick={onPat}>
+        <Avatar className="shake" src={require('../assets/avatar.jpg')} alt="头像"/>
+      </div>
       <PatPat>"{patter}"拍了拍"{pattee}"{content}</PatPat>
     </Container>
   );
