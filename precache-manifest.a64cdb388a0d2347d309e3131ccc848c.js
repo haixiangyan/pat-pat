8ec438a0a3bba2cf9ee12eca808d3ed07d6e0804
tx-haixiang <haixiangyan@tencent.com>
2020-08-21 11:41:06 +0800

    Updates

---
+++ precache-manifest.a64cdb388a0d2347d309e3131ccc848c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e96924dd9d3dda65c4803bff194a96f",
+    "url": "/pat-pat/index.html"
+  },
+  {
+    "revision": "1c3f3097f495f77d50f1",
+    "url": "/pat-pat/static/css/main.36857685.chunk.css"
+  },
+  {
+    "revision": "9f1271d6f300bc5bf87d",
+    "url": "/pat-pat/static/js/2.1e4b02d8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pat-pat/static/js/2.1e4b02d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c3f3097f495f77d50f1",
+    "url": "/pat-pat/static/js/main.5e1ea94e.chunk.js"
+  },
+  {
+    "revision": "a5c99df97232353072c1",
+    "url": "/pat-pat/static/js/runtime-main.94448433.js"
+  },
+  {
+    "revision": "81c0f3ef6a52f678bc2757535ffe80a6",
+    "url": "/pat-pat/static/media/avatar.81c0f3ef.jpg"
+  }
+]);
